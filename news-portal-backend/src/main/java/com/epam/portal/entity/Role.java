724381d5bdb7cbf051f8b997ab5fd5b1c49f299f
Mikhail <davebottomhell@gmail.com>
2020-10-20 03:49:30 +0600

    Role and User entities added

---
+++ news-portal-backend/src/main/java/com/epam/portal/entity/Role.java
@@ -0,0 +1,46 @@
+package com.epam.portal.entity;
+
+import java.util.Objects;
+
+public class Role {
+
+    private long id;
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id == role.id &&
+                Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
