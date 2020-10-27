936e81867d5d3585d5f89081ffdf031ad672853e
Mikhail <davebottomhell@gmail.com>
2020-10-27 22:19:48 +0600

    NewsServiceImpl and NewsDAOImpl unit test covered

---
+++ news-portal-backend/src/test/com/epam/portal/repository/NewsDAOImplTest.java
@@ -0,0 +1,93 @@
+package com.epam.portal.repository;
+
+import com.epam.portal.entity.News;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.rules.ExpectedException;
+
+
+import javax.persistence.PersistenceException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.List;
+
+import static com.epam.portal.NewsTestData.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NewsDAOImplTest extends TestCase {
+
+    @Mock
+    private NewsDAO newsDAO;
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    public News news;
+    public LocalDateTime testDate;
+
+    @Before
+    public void setUp() throws Exception {
+        news = new News(TEST_TEXT, TEST_TEXT, TEST_TEXT, testDate);
+        news.setId(1L);
+        testDate = LocalDateTime.parse(TEST_STRING_DATE, FORMATTER);
+
+    }
+
+    @Test
+    public void testSaveOrUpdateNews_WhenEverythingIsOk() {
+        long testId = 1L;
+        when(newsDAO.saveOrUpdateNews(news)).thenReturn(testId);
+        long result = newsDAO.saveOrUpdateNews(news);
+        Assert.assertEquals(result, testId);
+    }
+
+    @Test
+    public void testSaveOrUpdateNews_WhenThrowsBusinessException() {
+        exceptionRule.expect(PersistenceException.class);
+        DateTimeFormatter wrongFormatter = DateTimeFormatter.ofPattern("yyyy-dd-MM");
+        LocalDateTime wrongFormatDate = LocalDateTime.now();
+        wrongFormatDate.format(wrongFormatter);
+        news.setNewsDate(wrongFormatDate);
+        when(newsDAO.saveOrUpdateNews(news)).thenThrow(new PersistenceException());
+        newsDAO.saveOrUpdateNews(news);
+    }
+
+    @Test
+    public void testGetAllNews_WhenEverythingIsOk() {
+        List<News> testNewsList = Collections.emptyList();
+        when(newsDAO.getAllNews()).thenReturn(testNewsList);
+        List<News> resultList = newsDAO.getAllNews();
+        Assert.assertEquals(testNewsList, resultList);
+    }
+
+    @Test
+    public void testDeleteNews_WhenEverythingIsOk() {
+        long testId = 1L;
+        doNothing().when(newsDAO).deleteNews(testId);
+        newsDAO.deleteNews(testId);
+    }
+
+    @Test
+    public void testDeleteNews_WhenThrowsNPE() {
+        long testId = 1L;
+        exceptionRule.expect(NullPointerException.class);
+        doThrow(new NullPointerException()).when(newsDAO).deleteNews(testId);
+        newsDAO.deleteNews(testId);
+    }
+
+    @Test
+    public void testGetNewsById_WhenEverythingIsOk() {
+        long testId = 1L;
+        when(newsDAO.getNewsById(testId)).thenReturn(news);
+        News result = newsDAO.getNewsById(testId);
+        Assert.assertEquals(testId, result.getId());
+    }
+}
---
+++ news-portal-backend/src/test/com/epam/portal/service/NewsServiceImplTest.java
@@ -0,0 +1,121 @@
+package com.epam.portal.service;
+
+import com.epam.portal.dto.NewsDTO;
+import com.epam.portal.entity.News;
+import com.epam.portal.exception.BusinessException;
+import com.epam.portal.repository.NewsDAO;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static com.epam.portal.NewsTestData.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NewsServiceImplTest {
+
+    @InjectMocks
+    private NewsServiceImpl newsService;
+
+    @Mock
+    private NewsDAO newsDAO;
+
+    public NewsDTO newsDTO;
+    public News news;
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Before
+    public void init() {
+        newsDTO = new NewsDTO(NEWS_ID_1, TEST_TEXT, TEST_TEXT,TEST_TEXT, TEST_STRING_DATE);
+        news = new News(TEST_TEXT, TEST_TEXT,TEST_TEXT, testDate);
+        news.setId(1L);
+    }
+
+    @Test
+    public void testSaveOrUpdateNews_WhenEverythingIsOk() throws BusinessException {
+        long testId = 1L;
+        when(newsDAO.saveOrUpdateNews(news)).thenReturn(testId);
+        long result = newsService.saveOrUpdateNews(newsDTO);
+        Assert.assertEquals(result, testId);
+    }
+
+    @Test
+    public void testSaveOrUpdateNews_WhenThrowsBusinessException() throws BusinessException {
+        exceptionRule.expect(BusinessException.class);
+        exceptionRule.expectMessage("Not valid format of date.");
+        newsDTO.setNewsDate("wrong date format");
+        newsService.saveOrUpdateNews(newsDTO);
+    }
+
+    @Test
+    public void testGetNewsById_WhenEverythingIsOk() {
+        long testId = 1L;
+        when(newsDAO.getNewsById(testId)).thenReturn(news);
+        NewsDTO resultNewsDTOFromService = newsService.getNewsById(testId);
+        Assert.assertEquals(testId, resultNewsDTOFromService.getId());
+    }
+
+    @Test
+    public void testGetNewsById_WhenThrowsNPE() {
+        long testId = 1L;
+        exceptionRule.expect(NullPointerException.class);
+        when(newsDAO.getNewsById(testId)).thenThrow(new NullPointerException());
+        newsService.getNewsById(testId);
+    }
+
+    @Test
+    public void testGetAllNews_WhenEverythingIsOk() {
+        List<News> testNewsList = Collections.emptyList();
+        List<NewsDTO> expectedNewsDTOList = Collections.emptyList();
+        when(newsDAO.getAllNews()).thenReturn(testNewsList);
+        List<NewsDTO> resultList = newsService.getAllNews();
+        Assert.assertEquals(expectedNewsDTOList, resultList);
+    }
+
+    @Test
+    public void testDeleteNews_WhenEverythingIsOk() {
+        long testId = 1L;
+        doNothing().when(newsDAO).deleteNews(testId);
+        newsService.deleteNews(testId);
+    }
+
+    @Test
+    public void testDeleteNews_WhenThrowsNPE() {
+        long testId = 1L;
+        exceptionRule.expect(NullPointerException.class);
+        doThrow(new NullPointerException()).when(newsDAO).deleteNews(testId);
+        newsService.deleteNews(testId);
+    }
+
+    @Test
+    public void testDeleteNewsList_WhenEverythingIsOk() {
+        long testId = 1L;
+        List<Long> testLongList = new ArrayList<>();
+        testLongList.add(testId);
+        doNothing().when(newsDAO).deleteNews(testId);
+        newsService.deleteNews(testLongList);
+    }
+
+    @Test
+    public void testDeleteNewsList_WhenNPE() {
+        long testId = 1L;
+        List<Long> testLongList = new ArrayList<>();
+        testLongList.add(testId);
+        exceptionRule.expect(NullPointerException.class);
+        doThrow(new NullPointerException()).when(newsDAO).deleteNews(testId);
+        newsService.deleteNews(testLongList);
+    }
+
+}
