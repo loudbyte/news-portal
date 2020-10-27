45e0e49dced30d07215210428c47fd16c1f2a29e
Mikhail <davebottomhell@gmail.com>
2020-10-27 18:30:02 +0600

    MainRestController unit test covered (except deleteNewsList)

---
+++ news-portal-backend/src/test/com/epam/portal/controller/MainRestControllerTest.java
@@ -0,0 +1,122 @@
+package com.epam.portal.controller;
+
+import com.epam.portal.dto.NewsDTO;
+import com.epam.portal.exception.BusinessException;
+import com.epam.portal.repository.NewsDAO;
+import com.epam.portal.service.NewsService;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import javax.persistence.PersistenceException;
+import java.util.Collections;
+
+import static com.epam.portal.NewsTestData.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class MainRestControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private NewsService newsService;
+
+    @Mock
+    private NewsDAO newsDAO;
+
+    @InjectMocks
+    private MainRestController controller;
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Before
+    public void before() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new MainRestController(newsService)).build();
+    }
+
+    @Test
+    public void testGetAllNews_WhenEverythingIsOk() throws Exception {
+        NewsDTO newsDTO = new NewsDTO(NEWS_ID_1, TEST_TEXT, TEST_TEXT, TEST_TEXT, TEST_STRING_DATE);
+        when(newsService.getAllNews()).thenReturn(Collections.singletonList(newsDTO));
+        mockMvc.perform(get("/api/news"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testGetNewsByI_WhenEverythingIsOkd() throws Exception {
+        long testId = 1L;
+        NewsDTO newsDTO = new NewsDTO(NEWS_ID_1, TEST_TEXT, TEST_TEXT, TEST_TEXT, TEST_STRING_DATE);
+        when(newsService.getNewsById(testId)).thenReturn(newsDTO);
+        mockMvc.perform(get("/api/news/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testGetNewsById_WhenThrowsNPE() throws Exception {
+        when(newsService.getNewsById(NEWS_ID_1)).thenThrow(new NullPointerException());
+        mockMvc.perform(get("/api/news/1"))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void saveOrUpdateNews_WhenEverythingIsOk() throws Exception {
+        NewsDTO newsDTO = new NewsDTO(NEWS_ID_1, TEST_TEXT, TEST_TEXT, TEST_TEXT, TEST_STRING_DATE);
+        when(newsService.saveOrUpdateNews(newsDTO)).thenReturn(NEWS_ID_1);
+        mockMvc.perform(post("/api/news")
+                .content("{}")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void saveOrUpdateNews_WhenReturnError415() throws Exception {
+        long testId = 1L;
+        NewsDTO newsDTO = new NewsDTO(NEWS_ID_1, TEST_TEXT, TEST_TEXT, TEST_TEXT, TEST_STRING_DATE);
+        when(newsService.saveOrUpdateNews(newsDTO)).thenReturn(testId);
+        mockMvc.perform(post("/api/news")
+                .content("")
+                .contentType(MediaType.TEXT_PLAIN))
+                .andExpect(status().is(415));
+    }
+
+    @Test
+    public void saveOrUpdateNews_WhenThrowsBusinessException() throws Exception {
+        exceptionRule.expect(BusinessException.class);
+        exceptionRule.expectMessage("Failed to create news");
+        NEWS_DTO_1.setNewsDate("wrong date format");
+        when(newsService.saveOrUpdateNews(NEWS_DTO_1)).thenThrow(new BusinessException("Failed to create news"));
+        controller.saveOrUpdateNews(NEWS_DTO_1);
+    }
+
+    @Test
+    public void deleteNewsById_WhenEverythingIsOk() throws Exception {
+        long testId = 1L;
+        doNothing().when(newsService).deleteNews(testId);
+        mockMvc.perform(delete("/api/news/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteNewsById_WhenThrowsBusinessException() throws Exception {
+        long testId = 1L;
+        exceptionRule.expect(BusinessException.class);
+        exceptionRule.expectMessage("News with that id not found");
+        when(controller.deleteNewsById(testId)).thenThrow(new PersistenceException());
+        controller.deleteNewsById(testId);
+    }
+
+}
