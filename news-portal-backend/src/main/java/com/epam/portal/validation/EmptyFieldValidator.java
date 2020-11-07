00a1d462f683696568c17a88c2ab1d2ffdd07154
Same Clone <sameclone@users.noreply.github.com>
2020-11-07 17:38:46 +0600

    Update EmptyFieldValidator.java

--- news-portal-backend/src/main/java/com/epam/portal/validation/EmptyFieldValidator.java
+++ news-portal-backend/src/main/java/com/epam/portal/validation/EmptyFieldValidator.java
@@ -10,6 +10,7 @@ public class EmptyFieldValidator {
     private static final String SPACE_REGEX = "^[ ]";
 
     public static boolean isNotEmptyField(String field) throws BusinessException {
+        //TODO there os better to use null==field insetad of 'field == null'
         if (field == null || field.equals(EMPTY_STRING) || Pattern.compile(SPACE_REGEX).matcher(field).find())
             throw new BusinessException("Empty field");
         return true;
