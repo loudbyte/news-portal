849bc166f512c07beb15fe1261e567a13fc1ba04
Mikhail <davebottomhell@gmail.com>
2020-11-08 23:51:56 +0600

    code review fixes in NewsDTOTextLengthValidator

--- news-portal-backend/src/main/java/com/epam/portal/validation/NewsDTOTextLengthValidator.java
+++ news-portal-backend/src/main/java/com/epam/portal/validation/NewsDTOTextLengthValidator.java
@@ -12,31 +12,43 @@ public class NewsDTOTextLengthValidator {
     private static final int CONTENT_LENGTH = 2000;
     private static final int DATE_LENGTH = 10;
 
-    public static boolean isTitleLengthValid(NewsDTO newsDTO) throws BusinessException {
-        if (newsDTO.getTitle().length() > TITLE_LENGTH)
-            throw new BusinessException("Invalid title length");
-
-        return true;
+    public static void isTitleLengthValid(NewsDTO newsDTO) throws BusinessException {
+        if (null != newsDTO) {
+            if (newsDTO.getTitle().length() > TITLE_LENGTH) {
+                throw new BusinessException("Invalid title length");
+            }
+        } else {
+            throw new BusinessException("Cannot reach news data");
+        }
     }
 
-    public static boolean isBriefLengthValid(NewsDTO newsDTO) throws BusinessException {
-        if (newsDTO.getBrief().length() > BRIEF_LENGTH)
-            throw new BusinessException("Invalid brief length");
-
-        return true;
+    public static void isBriefLengthValid(NewsDTO newsDTO) throws BusinessException {
+        if (null != newsDTO) {
+            if (newsDTO.getBrief().length() > BRIEF_LENGTH) {
+                throw new BusinessException("Invalid brief length");
+            }
+        } else {
+            throw new BusinessException("Cannot reach news data");
+        }
     }
 
-    public static boolean isContentLengthValid(NewsDTO newsDTO) throws BusinessException {
-        if (newsDTO.getContent().length() > CONTENT_LENGTH)
-            throw new BusinessException("Invalid content length");
-
-        return true;
+    public static void isContentLengthValid(NewsDTO newsDTO) throws BusinessException {
+        if (null != newsDTO) {
+            if (newsDTO.getContent().length() > CONTENT_LENGTH) {
+                throw new BusinessException("Invalid content length");
+            }
+        } else {
+            throw new BusinessException("Cannot reach news data");
+        }
     }
 
-    public static boolean isDateLengthValid(NewsDTO newsDTO) throws BusinessException {
-        if (newsDTO.getNewsDate().length() > DATE_LENGTH)
-            throw new BusinessException("Invalid date length");
-
-        return true;
+    public static void isDateLengthValid(NewsDTO newsDTO) throws BusinessException {
+        if (null != newsDTO) {
+            if (newsDTO.getNewsDate().length() > DATE_LENGTH) {
+                throw new BusinessException("Invalid date length");
+            }
+        } else {
+            throw new BusinessException("Cannot reach news data");
+        }
     }
 }
